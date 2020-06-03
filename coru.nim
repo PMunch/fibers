ad5b0622042663e9e8f0df31d309e2a43d2fd4b8
Peter Munch-Ellingsen <peterme@peterme.net>
2020-06-03 10:39:29 +0200

    Passing in context, returning future

--- coru.nim
+++ coru.nim
@@ -1,29 +1,58 @@
 type
-  Future = ref object
-    current: Test
-    next: Test
+  #Future = ref object
+  #  current: Test
+  #  next: Test
   Test = iterator () {.closure.}
+  AsyncContext = ref object
+    next: Test
+  Future[T] = ref object
+    retval: T
 
-proc createTest(): Future =
-  var res = new Future
-  res.current = iterator () =
-    echo "From test"
-    res.next = iterator() =
+proc createInner(cont: AsyncContext): Future[int] =
+  var res = new Future[int]
+  cont.next = iterator() =
+    echo "From inner"
+    res.retval = 42
+    yield
+    cont.next = iterator() =
       echo "From inner"
     yield
-    echo "Back in test"
+    cont.next = iterator() =
+      echo "From inner"
+    yield
+    cont.next = iterator() =
+      echo "From inner"
+    yield
+  return res
+
+
+proc createTest(cont: AsyncContext): Future[string] =
+  var res = new Future[string]
+  cont.next = iterator () =
+    echo "From test"
+    var fut = createInner(cont)
+    var next = cont.next
+    while not next.finished:
+      yield
+    echo "Back in test, returned: ", fut.retval
+    res.retval = "Return?"
   return res
 
 import lists
-var evQueue: DoublyLinkedList[Future]
-evQueue.append createTest()
+var
+  evQueue: DoublyLinkedList[Test]
+  asyncContext = new AsyncContext
+var res = asyncContext.createTest()
+evQueue.append asyncContext.next
 while evQueue.head != nil:
+  echo "tick"
   var first = evQueue.head
   evQueue.remove first
-  first.value.current()
-  if not first.value.current.finished:
-    if first.value.next != nil:
-      evQueue.append Future(current: first.value.next)
-      first.value.next = nil
+  first.value()
+  if asyncContext.next != nil:
+    evQueue.append asyncContext.next
+    asyncContext.next = nil
+  if not first.value.finished:
     evQueue.append first
 
+echo "Returned ", res.retval
